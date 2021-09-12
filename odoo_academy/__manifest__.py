35cdf94efda0577c962ed81e497683243d61c9e7
Jorge Aviña <sistemas@intralix.com>
2021-09-12 18:51:22 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy app to manage training""",
+    'description': """
+        Academy Module to manage Trainig:
+        - Courser
+        - Sessions
+        - Attendees
+    """,
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com',
+    'category': 'Training'
+    'version': '0.0.1',
+    'depends': ['base'],
+    
+    'data':[
+        
+    ],
+    
+    'demo': [
+        
+    ],    
+}
